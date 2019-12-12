e3fb352a408f197be4526de76c5dad3f89f0fd45
Adam Corbett <kehvarl@gmail.com>
2019-12-12 07:32:08 -0700

    Part 2 solution is very slow.

--- 12.2-NBody/NBody.py
+++ 12.2-NBody/NBody.py
@@ -1,3 +1,4 @@
+from math import gcd
 class Moon:
     def __init__(self, x, y, z, vx=0, vy=0, vz=0):
         self.x = x
@@ -21,6 +22,9 @@ class Moon:
 
     def total_energy(self):
         return self.potential_energy() * self.kinetic_energy()
+
+    def state(self):
+        return [self.x, self.y, self.z, self.vx, self.vy, self.vz]
 
     def __repr__(self):
         return "pos=<x= {}, y= {}, z= {}>, vel=<x= {}, y= {}, z= {}".format(self.x, self.y, self.z, self.vx, self.vy,
@@ -63,22 +67,50 @@ if __name__ == "__main__":
              (1, 2), (1, 3),
              (2, 3)]
 
-    for moon in moons:
-        print(moon)
+    x_ = []
+    y_ = []
+    z_ = []
+    repeat_x = None
+    repeat_y = None
+    repeat_z = None
+    loop_count = 0
 
-    for i in range(1000):
+    while repeat_x is None or repeat_y is None or repeat_z is None:
         for pair in pairs:
             a, b = pair
             Moon.apply_gravity(moons[a], moons[b])
 
+        x_state = ""
+        y_state = ""
+        z_state = ""
         for moon in moons:
             moon.tick()
+            x_state += str(moon.x) + str(moon.vx)
+            y_state += str(moon.y) + str(moon.vy)
+            z_state += str(moon.z) + str(moon.vz)
 
-    print()
-    TE = 0
-    for moon in moons:
-        print(moon)
-        print(moon.total_energy())
-        TE += moon.total_energy()
+        if x_state not in x_:
+            x_.append(x_state)
+        elif repeat_x is None:
+            repeat_x = loop_count
 
-    print(TE)
+        if y_state not in y_:
+            y_.append(y_state)
+        elif repeat_y is None:
+            repeat_y = loop_count
+
+        if z_state not in z_:
+            z_.append(z_state)
+        elif repeat_z is None:
+            repeat_z = loop_count
+
+        loop_count += 1
+
+    print(repeat_x, repeat_y, repeat_z)
+
+
+    def least_common_multiple(x, y):
+        return x // gcd(x, y) * y
+
+
+    print(least_common_multiple(least_common_multiple(repeat_x, repeat_y), repeat_z))
