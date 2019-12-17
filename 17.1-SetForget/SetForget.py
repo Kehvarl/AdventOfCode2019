774f1a96ab6586ead29a899e2fb079af3e6f0c8a
Adam Corbett <kehvarl@gmail.com>
2019-12-16 22:25:19 -0700

    Passes test case

---
+++ 17.1-SetForget/SetForget.py
@@ -0,0 +1,33 @@
+from IntCode import IntCode
+
+comp = IntCode([int(x) for x in open("input.txt", "r").readline().split(",")])
+comp.run()
+
+display = [chr(c) for c in comp.output]
+
+test_data = """..#..........
+..#..........
+#######...###
+#.#...#...#.#
+#############
+..#...#...#..
+..#####...^..""".split("\n")
+
+print(test_data)
+
+grid = [[test_data[y][x] for x in range(len(test_data[0]))] for y in range(len(test_data))]
+
+print(grid)
+intersections = []
+
+for y in range(1, len(test_data) - 1):
+    for x in range(1, len(test_data[0]) - 1):
+        if test_data[y][x] != ".":
+            if test_data[y - 1][x] != "." and \
+                    test_data[y + 1][x] != "." and \
+                    test_data[y][x - 1] != "." and \
+                    test_data[y][x + 1] != ".":
+                intersections.append(x * y)
+
+print(intersections)
+print(sum(intersections))
