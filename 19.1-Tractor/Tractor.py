52a8de3d48dd55c6cc19b1803240387617d39a97
Adam Corbett <kehvarl@gmail.com>
2019-12-18 22:42:54 -0700

    cos(theta) = A/B. and I know C = 100 Not sure how to find a,b...

--- 19.1-Tractor/Tractor.py
+++ 19.1-Tractor/Tractor.py
@@ -2,12 +2,15 @@ from IntCode import IntCode
 
 comp = IntCode([int(x) for x in open("input.txt", "r").readline().split(",")])
 
-width = 50
-height = 50
+width = 100
+height = 100
 
 grid = [["." for _ in range(width)] for _ in range(height)]
+slopes = []
 count = 0
 for y in range(height):
+    start = None
+    end = None
     for x in range(width):
         comp.reset()
         comp.input_val = [x, y]
@@ -17,7 +20,15 @@ for y in range(height):
             grid[y][x] = str(is_tractor)
             if is_tractor == 1:
                 count += 1
+                if start is None:
+                    start = x
+            if is_tractor == 0 and end is None and start is not None:
+                end = x
+        if y > 0 and start is not None and end is not None:
+            slopes.append((y, (start / y), (end / y)))
 
 for line in grid:
     print("".join(line))
 print(count)
+
+print(slopes)
