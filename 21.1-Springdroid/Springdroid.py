4a141a68a8c856371382700b8bbc19925b0ad8f0
Adam Corbett <kehvarl@gmail.com>
2019-12-20 22:46:13 -0700

    Top 200!!   Kind of cheated a bit...

--- 21.1-Springdroid/Springdroid.py
+++ 21.1-Springdroid/Springdroid.py
@@ -12,16 +12,27 @@ def send_instructions(instructions, computer):
 comp = IntCode([int(x) for x in open("input.txt", "r").readline().split(",")], input_val=[])
 comp.run()
 
-prog = ["NOT A J",
-        "NOT B T",
+prog = ["NOT A J",  # Hole in A
+
+        "NOT B T",  # Or in B
         "OR T J",
-        "NOT C T",
+
+        "NOT C T",  # Or in C
         "OR T J",
-        "AND D J",
-        "WALK"]
+
+        "AND D J",  # only if Safe landing in D
+
+        "NOT E T",  # Hole in E
+        "NOT T T",  # Actually Not a hole in E (Can walk to)
+
+        "OR H T",  # Or not a hole in H (Can Jump to)
+
+        "AND T J",
+
+        "RUN"]
 
 send_instructions(prog, comp)
 
 # out = [chr(char) for char in comp.output]
 print(comp.output)
-# print("".join(out))
+#print("".join(out))
