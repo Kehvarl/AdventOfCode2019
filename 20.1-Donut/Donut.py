5c0c2f3b5bee8802d4825088596304ec4cfe1eb7
Adam Corbett <kehvarl@gmail.com>
2020-01-01 15:04:23 -0700

    Building a graph of portals.

--- 20.1-Donut/Donut.py
+++ 20.1-Donut/Donut.py
@@ -1,85 +1,42 @@
-example1 = """         A           
-         A           
-  #######.#########  
-  #######.........#  
-  #######.#######.#  
-  #######.#######.#  
-  #######.#######.#  
-  #####  B    ###.#  
-BC...##  C    ###.#  
-  ##.##       ###.#  
-  ##...DE  F  ###.#  
-  #####    G  ###.#  
-  #########.#####.#  
-DE..#######...###.#  
-  #.#########.###.#  
-FG..#########.....#  
-  ###########.#####  
-             Z       
-             Z       """
+example1 = open("example1.txt", "r").read()
 
-grid = [[val for val in line] for line in example1.split("\n")]
+# grid = [[val for val in line] for line in example1.split("\n")]
+grid = example1.split("\n")
+length = 0
+
+for line in grid:
+    length = max(len(line), length)
+
+out = []
+for line in grid:
+    out.append(line[::-1].zfill(length)[::-1])
+print(out)
+
+grid = out
 scanned = []
 
-
-def check_adjacent(x, y, portal_id):
-    for _x, _y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        if y + _y > len(grid) - 1 or x + _x > len(grid[0]) - 1:
-            continue
-
-        if grid[y + _y][x + _x] == "." and len(portal_id) == 2:
-            return x + _x, y + _y, portal_id
-
-        elif grid[y + _y][x + _x].isalpha() and len(portal_id) == 1:
-            return check_adjacent(x + _x, y + _y, portal_id + grid[y + _y][x + _x])
-
-    return x, y, None
+neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 
-def in_portals(x, y):
-    for portal in portals:
-        for side in portals[portal]:
-            _x, _y = side
-            if (x, y) == (_x, _y):
-                return _x, _y
+def find_dot(x, y):
+    for (dx, dy) in neighbors:
+        if 0 <= x + dx < len(grid[0]) and 0 <= y + dy < len(grid):
+            if grid[y + dy][x + dx] == ".":
+                return (x + dx, y + dy), (x - dx, y - dy)  # (dot), (tag)
     return False
 
 
 portals = {}
-
+portal_links = {}
 for y in range(len(grid)):
     for x in range(len(grid[0])):
         if grid[y][x].isalpha():
-            _x, _y, portal = check_adjacent(x, y, grid[y][x])
-            if portal is not None:
-                portal = "".join(sorted(portal))
-                if portals.get(portal):
-                    portals[portal].append((_x, _y))
-                else:
-                    portals[portal] = [(_x, _y)]
+            portal = find_dot(x, y)
+            if portal:
+                dot, (tagx, tagy) = portal
+                tag = "".join(sorted(grid[y][x] + grid[tagy][tagx]))
+                if not portals.get(tag):
+                    portals[tag] = []
+                portals[tag].append(dot)
 
 print(portals)
-
-x, y = portals["AA"][0]
-path = [(x, y)]
-done = False
-
-while not done:
-    for _x, _y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        if y + _y > len(grid) - 1 or x + _x > len(grid[0]) - 1:
-            continue
-
-        if grid[y + _y][x + _x] == "." and (x, y) not in path:
-            x, y = [y + _y][x + _x]
-            path.append((x, y))
-
-        if in_portals(x, y):
-            if (x, y) == portals["AA"][0]:
-                continue
-            elif (x, y) == portals["ZZ"][0]:
-                done = True
-                break
-            x, y = in_portals(x, y)
-            path.append((x, y))
-
-print(path)
