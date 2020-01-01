51d2c00cd497888b3bce276e4ba88f32f9437b30
Adam Corbett <kehvarl@gmail.com>
2019-12-31 21:29:30 -0700

    What is being done doesn't quite click.  Another rewrite or two are needed

--- 18.1-ManyWorlds/Many2.py
+++ 18.1-ManyWorlds/Many2.py
@@ -35,16 +35,48 @@ def bfs_search(grid, start, havekeys):
     return keys_collected
 
 
+def reachablekeys(grid, start, havekeys):
+    bfs = collections.deque([start])
+    distance = {start: 0}
+    keys = {}
+    while bfs:
+        h = bfs.popleft()
+        for pt in [
+            (h[0] + 1, h[1]),
+            (h[0] - 1, h[1]),
+            (h[0], h[1] + 1),
+            (h[0], h[1] - 1),
+        ]:
+            if not (0 <= pt[0] < len(grid) and 0 <= pt[1] < len(grid[0])):
+                continue
+            ch = grid[pt[0]][pt[1]]
+            if ch == '#':
+                continue
+            if pt in distance:
+                continue
+            distance[pt] = distance[h] + 1
+            if 'A' <= ch <= 'Z' and ch.lower() not in havekeys:
+                continue
+            if 'a' <= ch <= 'z' and ch not in havekeys:
+                keys[ch] = distance[pt], pt
+            else:
+                bfs.append(pt)
+    return keys
+
+
 seen = {}
 
 
 def minwalk(grid, starts, havekeys):
+    keys = {}
     hks = ''.join(sorted(havekeys))
     if (starts, hks) in seen:
         return seen[starts, hks]
     if len(seen) % 10 == 0:
         print(hks)
-    keys = bfs_search(grid, starts, havekeys)
+    for i, start in enumerate(starts):
+        for ch, (dist, pt) in bfs_search(grid, start, havekeys).items():
+            keys[ch] = dist, pt, i
     if len(keys) == 0:
         # done!
         ans = 0
@@ -70,4 +102,4 @@ for i in range(len(maze)):
         if maze[i][j] == '@':
             starts.append((i, j))
 
-print(minwalk(maze, starts, ''))
+print(minwalk(maze, tuple(starts), ''))
